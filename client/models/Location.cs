718e113131e98e6b4e96593e4fc3e60acc648c6f
923-Feduș-Robert <52884970+robertfedus@users.noreply.github.com>
2024-04-09 00:47:27 +0300

    Location done 2

--- client/models/Location.cs
+++ client/models/Location.cs
@@ -8,14 +8,39 @@ namespace client.models
 {
     internal class Location
     {
-        private String googlePlacesPlaceId;
+        private string id;
+        private string name;
         private double latitude;
         private double longitude;
-        public Location(String googlePlacesPlaceId, double latitude, double longitude)
+
+        public Location(string id, String name, double latitude, double longitude)
         {
-            this.googlePlacesPlaceId = googlePlacesPlaceId;
+            this.id = id;
+            this.name = name;
             this.latitude = latitude;
             this.longitude = longitude;
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+        public double Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
         }
     }
 }
