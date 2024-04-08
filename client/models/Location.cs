b0bdfbaf46f8d5c73ef4dea9310c31b25ec955e8
923-Feduș-Robert <52884970+robertfedus@users.noreply.github.com>
2024-04-08 19:53:01 +0300

    Test for Location branch

---
+++ client/models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.models
+{
+    internal class Location
+    {
+        private String googlePlacesPlaceId;
+        private double latitude;
+        private double longitude;
+        public Location(String googlePlacesPlaceId, double latitude, double longitude)
+        {
+            this.googlePlacesPlaceId = googlePlacesPlaceId;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+    }
+}
