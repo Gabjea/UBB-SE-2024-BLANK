e407cb24c4e482c88fcc39e7f4e01b441c12ecf1
Florin <florinelgaina@yahoo.com>
2024-04-08 23:38:47 +0300

    Photo Compression Done!

--- client/models/PhotoMedia.cs
+++ client/models/PhotoMedia.cs
@@ -1,13 +1,4 @@
-﻿using client.models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Markup;
-
-namespace client.models
+﻿namespace client.models
 {
     class PhotoMedia : Media
     {
--- client/modules/EncryptionModule.cs
+++ client/modules/EncryptionModule.cs
@@ -1,10 +1,5 @@
 ﻿using client.models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace client.modules
 {
