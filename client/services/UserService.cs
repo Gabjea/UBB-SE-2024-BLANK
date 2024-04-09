f8d17d0087abf3d6407dfdccdabac311c8c877e7
923-Feduș-Robert <52884970+robertfedus@users.noreply.github.com>
2024-04-10 02:01:32 +0300

    User logic done

---
+++ client/services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using client.repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using client.models;
+
+namespace client.services
+{
+    class UserService
+    {
+        private UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User getUserById(Guid id)
+        {
+            return _userRepository.getUserById(id);
+        }
+    }
+}
